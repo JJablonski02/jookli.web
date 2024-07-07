acce0afc976b62d8727987ff4f11ed684ba057f7
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-07 21:34:42 +0200

    JPS-2 prepared sections with comments to code it later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FaqSection from "@/components/FaqSection/Faq";
 import Button from "@/components/button";
 import { decodeFormState } from "next/dist/server/app-render/entry-base";
 import Image from "next/image";
@@ -5,7 +6,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <header className="container mx-16 mt-14 grid h-full w-full place-items-center grid-cols-2">
+      <header className=" mx-6 mt-14 grid h-full w-full place-items-center grid-cols-2">
         <div>
           <h1 className="text-5xl text-primary">
             Lorem ipsum <br /> dolor sit amet.
@@ -32,16 +33,26 @@ export default function Home() {
           />
         </div>
       </header>
-      <div className="py-10 px-10 mx-8 mt-20 border-2 border-black rounded-lg ">
+      <div className=" py-10 px-10 mx-auto max-w-7xl mt-20 border-2 border-black rounded-lg ">
         <div>
           <h3 className="text-3xl">lorem</h3>
-          <p>
+          <p className="w-2/3 pt-4">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio
             eveniet harum magni repellendus totam ipsam quae veniam consequuntur
             magnam molestiae.
           </p>
         </div>
       </div>
+      <div className="p-8">
+        <div className="mx-auto mt-20 border-2 rounded text-center max-w-6xl">
+          <img src=""></img>
+          <button>x</button>
+        </div>
+      </div>
+      <section>{/* appinfo */}</section>
+      <section>{/* mc */}</section>
+      <section>{/* wcl */}</section>
+      <FaqSection />
     </>
   );
 }
