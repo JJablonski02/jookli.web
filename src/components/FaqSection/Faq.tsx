0cfe6ccad3a46a768304a93f4a089ecc5c4a5c59
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-08 19:02:01 +0200

    added wrapper to FaqSection

--- src/components/FaqSection/Faq.tsx
+++ src/components/FaqSection/Faq.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { FaqData } from "./FaqData";
+import WidthWrapper from "../WidthWrapper";
 
 const FaqSection = () => {
   return (
-    <section className="max-w-6xl mx-auto">
-      <div className="flex justify-center flex-col items-center text-center py-10 ">
-        <div className="">
-          <h2 className="text-5xl">FAQ</h2>
-          <p className="text-xl pt-4">random paragraph</p>
+    <WidthWrapper>
+      <section>
+        <div className="flex justify-center flex-col items-center text-center py-10 ">
+          <div className="">
+            <h2 className="text-5xl">FAQ</h2>
+            <p className="text-xl pt-4">random paragraph</p>
+          </div>
+          <div className="grid grid-cols-3 gap-20 mt-10 ">
+            {FaqData.map((data) => (
+              <div className="text-start">
+                <h3 className="text-xl">{data.header}</h3>
+                <p className="pt-6 "> {data.content}</p>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="grid grid-cols-3 gap-20 mt-10 ">
-          {FaqData.map((data) => (
-            <div className="text-start">
-              <h3 className="text-xl">{data.header}</h3>
-              <p className="pt-6 "> {data.content}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
+      </section>
+    </WidthWrapper>
   );
 };
 
