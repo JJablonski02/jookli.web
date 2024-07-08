917bb525ed08c8442ec2003e18787e66ab1e645f
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-08 17:50:17 +0200

    added padding to new div

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,22 +4,24 @@ import JPButton from "./JPButton";
 
 const Header = () => {
   return (
-    <header className="mx-8 mt-14 grid place-items-center grid-cols-2">
-      <div className="">
-        <h1 className="text-5xl text-primary">
-          Lorem ipsum <br /> dolor sit amet.
-        </h1>
-        <p className="mt-10 text-xl font-semibold">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur,
-          nulla.
-        </p>
-        <h6 className="text-base mt-6">Get the app</h6>
-        <div className="flex gap-4 mt-4">
-          <JPButton label="Get the app" />
-          <JPButton label="Get the app" />
+    <header className="mx-8 mt-14">
+      <div className="px-6 grid place-items-center grid-cols-2">
+        <div>
+          <h1 className="text-5xl text-primary">
+            Lorem ipsum <br /> dolor sit amet.
+          </h1>
+          <p className="mt-10 text-xl font-semibold">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consectetur, nulla.
+          </p>
+          <h6 className="text-base mt-6">Get the app</h6>
+          <div className="flex gap-4 mt-4">
+            <JPButton label="Get the app" />
+            <JPButton label="Get the app" />
+          </div>
         </div>
+        <Image src="next.svg" alt="Home Image" width={400} height={500} />
       </div>
-      <Image src="next.svg" alt="Home Image" width={400} height={500} />
     </header>
   );
 };
