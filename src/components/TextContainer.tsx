8c3d7d21ae04774e8489ed07df39c7e126e713af
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-09 00:01:42 +0200

    cosmetic change

--- src/components/TextContainer.tsx
+++ src/components/TextContainer.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 
 const TextContainer: React.FC<ContainerProps> = ({ header, content }) => {
   return (
-    <div className=" py-10 px-10 mx-8 mt-20 border-2 border-black rounded-lg shadow-md ">
+    <div className="p-10 mx-8 mt-20 border-2 border-black rounded-lg shadow-md ">
       <h3 className="text-3xl">{header}</h3>
       <p className="w-2/3 pt-4">{content}</p>
     </div>
