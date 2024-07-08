5674a393ce153fbc2b25583a81a4452445123d08
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-08 23:50:34 +0200

    moved banner video to new component

---
+++ src/components/BannerVideo.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import JPButton from "@/components/JPButton";
+import WidthWrapper from "./WidthWrapper";
+
+const BannerVideo = () => {
+  return (
+    <WidthWrapper>
+      <div className="p-8">
+        <div className="mx-auto mt-20 border-2 rounded-xl text-center max-w-6xl relative flex justify-center items-center">
+          <Image
+            className="w-full h-full"
+            src="next.svg"
+            alt="Home Image"
+            width={200}
+            height={200}
+          />
+          <div className="absolute flex justify-center items-center h-full w-full">
+            <JPButton className="rounded-full" label="x" />
+          </div>
+        </div>
+      </div>
+    </WidthWrapper>
+  );
+};
+
+export default BannerVideo;
