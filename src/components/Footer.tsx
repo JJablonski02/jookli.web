770a627da0abe6a1781fdc979ec356ce72df774c
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-13 18:21:43 +0200

    JPS-19 improved visibility of footer on devices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,43 +7,45 @@ import JPButton from "./JPButton";
 
 const Footer = () => {
   return (
-    <footer className="mt-20 px-20  bg-secondary ">
-      <div className="flex justify-between items-center pt-4">
-        <div className="">
-          <div className="w-fit">
-            <Link href="/">
-              <Image
-                className="my-4"
-                src={LogoSvg}
-                alt="Logo jookli website"
-                width={100}
-              />
-            </Link>
+    <footer className="mt-20 px-20 bg-secondary">
+      <div className="max-w-7xl mx-auto ">
+        <div className="flex justify-between items-center pt-4 md:flex-col lg:justify-around ">
+          <div className="">
+            <div className="w-fit">
+              <Link href="/">
+                <Image
+                  className="my-4"
+                  src={LogoSvg}
+                  alt="Logo jookli website"
+                  width={100}
+                />
+              </Link>
+            </div>
+            <p className="text-black mb-8">Test</p>
+            <ul className="flex items-center justify-start gap-4 pt-5 lg:flex-col lg:items-start md:items-center md:mb-10 ">
+              <li>
+                <Link href={toHome}>Strona główna</Link>
+              </li>
+              <li>
+                <Link href={toHowItWorks}>Na czym to polega</Link>
+              </li>
+              <li>
+                <Link href={toPaymentMethods}>Metody zarabiania</Link>
+              </li>
+              <li>
+                <Link href={toSupport}>Wsparcie</Link>
+              </li>
+            </ul>
           </div>
-          <p className="text-black mb-8">Test</p>
-          <ul className="flex items-center justify-start gap-4 pt-5 ">
-            <li>
-              <Link href={toHome}>Strona główna</Link>
-            </li>
-            <li>
-              <Link href={toHowItWorks}>Na czym to polega</Link>
-            </li>
-            <li>
-              <Link href={toPaymentMethods}>Metody zarabiania</Link>
-            </li>
-            <li>
-              <Link href={toSupport}>Wsparcie</Link>
-            </li>
-          </ul>
+          <div className="flex flex-col gap-4">
+            <h6 className="">Get the app</h6>
+            <JPButton label="get the app app" />
+            <JPButton label="get the app app" />
+          </div>
         </div>
-        <div className="flex flex-col gap-4">
-          <h6 className="">Get the app</h6>
-          <JPButton label="get the app app" />
-          <JPButton label="get the app app" />
+        <div className="mt-12 py-6 border-t border-gray-500">
+          <p className="text-black">© 2024 by ...</p>
         </div>
-      </div>
-      <div className="mt-12 py-6 border-t border-gray-500">
-        <p className="text-black">© 2024 by ...</p>
       </div>
     </footer>
   );
