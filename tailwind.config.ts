6626f6120fd2ffbd639653f8701d2914b4a7c8e0
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-13 17:43:17 +0200

    added max screen sizes to config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,13 @@ module.exports = {
         "2xl": "1400px",
       },
     },
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },
     darkMode: "class",
     plugins: [nextui()],
     extend: {
