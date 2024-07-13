b9fd68e57e1d89cd96a1491706024ba364513b10
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-13 17:09:41 +0200

    JPS-2 created page wrapper, added to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,23 @@ import SectionWithImage from "@/components/SectionWithImage/SectionWithImage";
 import InfoSection from "@/components/InfoSection/InfoSection";
 import BannerVideo from "@/components/BannerVideo";
 import TextContainer from "@/components/TextContainer";
+import PageWrapper from "@/components/PageWrapper";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <TextContainer
-        header="header"
-        content="content Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, maiores."
-      />
-      <BannerVideo />
-      <InfoSection />
-      <SectionWithImage />
-      <WCSSection />
-      <FaqSection />
+      <PageWrapper>
+        <Header />
+        <TextContainer
+          header="header"
+          content="content Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet, maiores."
+        />
+        <BannerVideo />
+        <InfoSection />
+        <SectionWithImage />
+        <WCSSection />
+        <FaqSection />
+      </PageWrapper>
     </>
   );
 }
---
+++ src/components/PageWrapper.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactNode } from "react";
+
+const PageWrapper = ({ children }: { children: ReactNode }) => {
+  return <main className="max-w-[1920px] mx-auto">{children}</main>;
+};
+
+export default PageWrapper;
