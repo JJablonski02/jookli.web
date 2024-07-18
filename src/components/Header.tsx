909de88de3c3105db9d9c06d3e8175d384788480
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-19 00:32:14 +0200

    added translation and button from nextui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import Image from "next/image";
-import JPButton from "./JPButton";
+import { Button, Link } from "@nextui-org/react";
+import { useTranslations } from "next-intl";
 
 const Header = () => {
+  const t = useTranslations("Header");
 
   return (
     <header className="mx-8 mt-14">
       <div className="px-6 flex justify-between items-center md:flex-col">
         <div className="md:mb-10">
-          <h1 className="text-5xl">
-            Lorem ipsum <br /> dolor sit amet.
-          </h1>
+          <h1 className="text-5xl w-1/2">{t("headerDescription")}</h1>
           <p className="mt-10 text-xl font-semibold w-3/4">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             Consectetur, nulla.
           </p>
-          <h6 className="text-base mt-6">Get the app</h6>
           <div className="flex gap-4 mt-4">
-            <JPButton label="Get the app" />
-            <JPButton label="Get the app" />
+            <Button
+              className="text-primary bg-blue"
+              radius="full"
+              as={Link}
+              href=""
+            >
+              {t("downloadApp")}
+            </Button>
           </div>
         </div>
         <div>
