d2c1c8ea59f4c3f5852105d0c61381b1a544270e
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-19 16:41:58 +0200

    used renamed component in homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,15 +2,15 @@ import FaqSection from "@/components/FaqSection/Faq";
 import Header from "@/components/Header";
 import WCSSection from "@/components/WCSSection/WCSSection";
 import SectionWithImage from "@/components/SectionWithImage/SectionWithImage";
-import InfoSection from "@/components/InfoSection/InfoSection";
 import PageWrapper from "@/components/PageWrapper";
+import AboutSection from "@/components/AboutSection/AboutSection";
 
 export default function Home() {
   return (
     <>
       <PageWrapper>
         <Header />
-        <InfoSection />
+        <AboutSection />
         <SectionWithImage />
         {/* <SectionWithImage /> musi zostac w  komponencie zmieniony tekst z obrazkiem  */}
         <WCSSection />
