a99d621904c941ec0d6c33578c455bd727ea8adf
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-28 21:16:44 +0200

    created tile for download section

---
+++ src/components/DownloadSection/Tile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../../../public/logo.svg";
+
+interface TileProps {
+  header: string;
+  description: string;
+  children: React.ReactNode;
+}
+
+const Tile: React.FC<TileProps> = ({ header, description, children }) => {
+  return (
+    <div className="bg-white rounded-2xl">
+      <div className="flex flex-col items-start  p-8">
+        <h3 className="text-black text-2xl mb-4 font-PoppinsMedium">
+          {header}
+        </h3>
+        <p className="text-black text-sm mb-4">{description}</p>
+        {children}
+        <Image src={logo} alt="icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Tile;
