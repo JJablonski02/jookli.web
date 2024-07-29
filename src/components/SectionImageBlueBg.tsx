74d3fb79e6bfb3505e71d552a1c89b15caffa0a3
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-07-29 23:37:35 +0200

    JPS-13 added styles to section component to be responsive

--- src/components/SectionImageBlueBg.tsx
+++ src/components/SectionImageBlueBg.tsx
@@ -8,23 +8,23 @@ import { useTranslations } from "next-intl";
 const SectionImageBlueBg = () => {
   const t = useTranslations("SectionImageBlueBg");
   return (
-    <section className="bg-blue p-6">
+    <section className="bg-blue py-6 lg:py-2">
       <WidthWrapper>
         <div className="py-10 m-10 flex justify-evenly items-center text-center md:flex md:flex-col gap-4">
           <div>
-            <div className="flex justify-start flex-col w-3/4">
+            <div className="flex justify-start md:items-center flex-col w-3/4 md:w-full">
               <h2 className="text-xl text-primary text-start">
                 {t("headerDescription")}
               </h2>
-              <p className="mt-10 text-start font-semibold">
+              <p className="mt-10 text-start md:text-center font-semibold">
                 {t("paragraphDescription")}
               </p>
               <div className="flex justify-start">
-                <JPDownloadButton className="text-blue bg-white mt-10" />
+                <JPDownloadButton className="text-blue bg-white mt-10 md:mt-6" />
               </div>
             </div>
           </div>
-          <div className="md:mb-10">
+          <div className="md:mt-6">
             <Image src={logo} alt="Home Image" width={400} height={500} />
           </div>
         </div>
--- src/components/SectionWithImage.tsx
+++ src/components/SectionWithImage.tsx
@@ -12,18 +12,18 @@ const SectionWithImage = () => {
   return (
     <section>
       <WidthWrapper>
-        <div className="py-10 m-10 flex justify-evenly items-center md:flex md:flex-col gap-4 text-start">
+        <div className="mx-10 my-16 lg:mt-10 mb-20 flex justify-evenly items-center md:flex md:flex-col gap-4 text-start">
           <div className="md:mb-10">
             <Image src={logo} alt="logo svg" width={400} height={500} />
           </div>
           <div>
             <div className="w-3/4">
               <h2 className="text-xl text-primary">{t("headerDescription")}</h2>
-              <p className="mt-10 text-base font-semibold">
+              <p className="mt-10 text-base font-semibold lg:mt-6">
                 {t("paragraphDescription")}
               </p>
             </div>
-            <div className="flex flex-col pt-8 gap-4 place-items-start">
+            <div className="flex flex-col pt-8 lg:pt-4 gap-4 place-items-start">
               <div className="flex justify-center items-center flex-row">
                 <div className="p-2 rounded-xl bg-blue mr-4">
                   <Image
