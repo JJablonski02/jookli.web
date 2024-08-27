e6ec54b733acbdec6dfb42e5770f0f40a342d41c
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-27 08:06:03 +0200

    changed images and added translations

--- src/components/AboutSection/WhiteTile.tsx
+++ src/components/AboutSection/WhiteTile.tsx
@@ -11,14 +11,16 @@ interface whiteTileProps {
 const WhiteTile: React.FC<whiteTileProps> = ({ header, content, src, alt }) => {
   return (
     <div className="w-full h-full rounded-[40px] bg-primary">
-      <div className="flex flex-col flex-start m-6 py-2">
-        <Image
-          className="p-2 bg-blue rounded-xl"
-          width={50}
-          src={src}
-          alt={alt}
-        />
-        <h3 className="text-black">{header}</h3>
+      <div className="flex flex-col flex-start m-6">
+        <div className="flex flex-row items-center gap-[20px] mb-[8px]">
+          <Image
+            className="bg-blue rounded-3xl"
+            width={68}
+            src={src}
+            alt={alt}
+          />
+          <h3 className="text-black">{header}</h3>
+        </div>
         <p className="text-black">{content}</p>
       </div>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,28 @@
 import React from "react";
-import Image from "next/image";
+import phoneImage from "../../public/phoneImage.png";
 import { useTranslations } from "next-intl";
 import JPDownloadButton from "./JPDownloadButton";
+import Image from "next/image";
 
 const Header = () => {
   const t = useTranslations("Header");
 
   return (
     <header className="mx-8 mt-14 md:mt-10 md:mx-2.5">
-      <div className="px-6 md:px-0 flex justify-between items-center md:flex-col ">
+      <div className="px-6 md:px-0 flex justify-between items-center md:flex-col">
         <div className="md:flex md:items-center md:flex-col">
-          <h1 className="text-5xl w-1/2 md:w-full">{t("headerDescription")}</h1>
+          <h1 className="text-5xl w-10/12 md:w-full">
+            {t("headerDescription")}
+          </h1>
           <p className="mt-10 text-xl font-semibold w-1/2 md:mt-6 md:w-full">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Consectetur, nulla.
+            {t('headerParagraph')}
           </p>
-          <div className="mt-4 md:mx-auto md:mt-8 md:w-10/12">
+          <div className="w-1/2 mt-8 md:mx-auto md:mt-8 md:w-10/12">
             <JPDownloadButton className="md:w-full md:h-14 text-primary md:text-xl bg-blue" />
           </div>
         </div>
         <div className="lg:hidden">
-          <Image src="next.svg" alt="Home Image" width={400} height={500} />
+          <Image src={phoneImage} alt="Phone image" width={400} height={500} />
         </div>
       </div>
     </header>
