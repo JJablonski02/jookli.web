88f72c54c6489320535c89192579036c8f8b3704
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-29 16:18:58 +0200

    moved faq section to component folder

--- src/components/FaqSection/Faq.tsx
+++ src/components/FaqSection/Faq.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaqData } from "./FaqData";
-import WidthWrapper from "../../WidthWrapper";
+import WidthWrapper from "../WidthWrapper";
 
 const FaqSection = () => {
   return (
