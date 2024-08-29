f066f6c64c8454f179ecf1919cfc4e17d4b66913
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-29 21:35:56 +0200

    added margin to header, buttons

--- src/components/HowItWorksComponents/Header.tsx
+++ src/components/HowItWorksComponents/Header.tsx
@@ -3,12 +3,13 @@ import phoneImage from "../../../public/phoneImage.png";
 import { useTranslations } from "next-intl";
 import JPDownloadButton from "../JPDownloadButton";
 import Image from "next/image";
+import JPButton from "../JPButton";
 
 const Header = () => {
   const t = useTranslations("howItWorksPage.Header");
 
   return (
-    <header className="mx-8 mt-14 md:mt-10 md:mx-2.5 relative">
+    <header className="mx-20 mt-14 md:mt-10 md:mx-2.5 relative">
       <div className="-z-10 lg:hidden">
         <span className="border-[5px] border-blue w-[381px] h-[381px] absolute rounded-[54px] rotate-45 -right-40 top-16"></span>
         <span className="border-[5px] border-blue w-[381px] h-[381px] absolute rounded-[54px] rotate-45 -right-60 top-16"></span>
@@ -18,16 +19,22 @@ const Header = () => {
           <h1 className="text-5xl w-2/3 md:w-full">
             {t("headerDescription")}
           </h1>
-          <p className="mt-10 text-lg w-2/3 md:mt-6 md:w-full">
+          <p className="mt-10 text-md w-2/3 md:mt-6 md:w-full">
             {t('headerParagraph')}
           </p>
-          <div className="w-1/2 mt-8 md:mx-auto md:mt-8 md:w-10/12">
-            <JPDownloadButton className="md:w-full md:h-14 text-primary md:text-xl bg-blue" />
-            <JPDownloadButton className="md:w-full md:h-14 text-primary md:text-xl bg-blue" />
+          <div className="w-2/3 mt-8 md:mx-auto md:mt-8 md:w-10/12 flex justify-start gap-6">
+            <JPButton
+                  className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
+                  label={t('downloadIos').toUpperCase()}
+              />
+            <JPButton
+                  className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
+                  label={t('downloadAndroid').toUpperCase()}
+              />
           </div>
         </div>
-        <div className="lg:hidden z-10 relative right-12">
-          <Image src={phoneImage} alt="Phone image" width={343} height={677} />
+        <div className="lg:hidden">
+          <Image className="z-10 relative right-12" src={phoneImage} alt="Phone image" width={343} height={677} />
         </div>
       </div>
       
