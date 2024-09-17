65ef281f6849763fa03a8788a98b066637a215f7
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-09-17 08:26:01 +0200

    deleted pagination from next ui

--- src/components/HomePageComponents/WCSSection/test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {Pagination} from "@nextui-org/react";
-
-export default function Paggination() {
-  return (
-    <Pagination total={6} initialPage={1} />
-  );
-}
