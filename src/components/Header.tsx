8d29f069c1ce5ba6767c5cf37b43af270509131f
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-09-17 08:31:24 +0200

    added to interface an img option

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,15 @@ import Image, { StaticImageData } from "next/image";
 import JPButton from "./JPButton";
 
 interface HeaderProps {
-    header: string;
-    headerParagraph: string;
-    firstButtonLabel: string;
-    secondButtonLabel: string;
-    src: StaticImageData;
-    secondSrc?: StaticImageData;
+  header: string;
+  headerParagraph: string;
+  firstButtonLabel: string;
+  secondButtonLabel: string;
+  src?: StaticImageData;
+  secondSrc?: StaticImageData;
 }
 
-const Header: React.FC<HeaderProps> = ({header,headerParagraph, firstButtonLabel, secondButtonLabel,src, secondSrc}) => {
+const Header: React.FC<HeaderProps> = ({ header, headerParagraph, firstButtonLabel, secondButtonLabel, src, secondSrc }) => {
 
   return (
     <header className="mx-20 mt-14 md:mt-10 md:mx-2.5 relative">
@@ -24,28 +24,30 @@ const Header: React.FC<HeaderProps> = ({header,headerParagraph, firstButtonLabel
           <h1 className="text-5xl w-10/12 md:w-full">
             {header}
           </h1>
-          <p className="mt-10 text-lg w-2/3 md:mt-6 md:w-full">
+          <p className="mt-8 text-lg w-2/3 md:mt-6 md:w-full">
             {headerParagraph}
           </p>
-          <div className="w-2/3 mt-8 md:mx-auto md:mt-8 md:w-10/12 flex justify-start gap-6">
+          <div className="w-2/3 mt-10 md:mx-auto md:mt-8 md:w-10/12 flex justify-start gap-6">
             <JPButton
-                  className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
-                  label={firstButtonLabel}
-              />
+              className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
+              label={firstButtonLabel}
+            />
             <JPButton
-                  className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
-                  label={secondButtonLabel}
-              />
+              className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
+              label={secondButtonLabel}
+            />
           </div>
         </div>
         <div className="lg:hidden">
-          <Image src={src} alt="Phone image" width={343} height={677} />
-          {secondSrc && 
-            <Image className="absolute -top-12 right-44 -z-10" src={src} alt="Phone image" width={343} height={677}/>
+          {src &&
+            <Image src={src} alt="Phone image" width={343} height={677} />
+          }
+          {secondSrc &&
+            <Image className="absolute -top-12 right-44 -z-10" src={secondSrc} alt="Phone image" width={343} height={677} />
           }
         </div>
       </div>
-      
+
     </header>
   );
 };
