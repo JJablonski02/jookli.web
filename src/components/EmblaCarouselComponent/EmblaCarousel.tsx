d1b513e38e1b0590fb6088c7a5e75a43b84a28c4
JJablonski02 <kuba2805@icloud.com>
2024-09-23 22:20:49 +0200

    Embla fix

--- src/components/EmblaCarouselComponent/EmblaCarousel.tsx
+++ src/components/EmblaCarouselComponent/EmblaCarousel.tsx
@@ -8,7 +8,7 @@ import user from '@/../public/user.svg'
 import { WCSData } from '../HomePageComponents/WCSSection/WCSData'
 
 export function EmblaCarousel() {
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ playOnInit: true })])
+  const [emblaRef] = useEmblaCarousel({ loop: true, slidesToScroll: 'auto' }, [Autoplay({ playOnInit: true })])
   const t = useTranslations();
 
   return (
@@ -16,17 +16,18 @@ export function EmblaCarousel() {
       <div className="embla__viewport overflow-hidden" ref={emblaRef}>
         <div className="embla__container">
           {WCSData.map((data, index) => (
-            <div key={index} className="embla__slide__active bg-white">
-              <div className="flex justify-between">
+            <div key={index} className="embla__slide">
+              <div className="bg-white shadow-inner rounded-[1.8rem] w-full min-w-0 p-8 h-[var(--slide-height)] select-none">              
+                <div className="flex justify-between items-center mb-4">
                 <span className="flex flex-row gap-2">
                   <p className="text-xl text-secondary lg:text-md">{data.name}</p>
                   <p className="text-xl text-secondary lg:text-md">{data.country}</p>
                 </span>
-
                 <Image src={user} alt={'user-svg'} className="w-14 h-14 lg:hidden" />
               </div>
               <div>
                 <p className="overflow-ellipsis text-secondary text-lg lg:text-sm">{`${t(`HomePage.WCSSection.${data.content}`)}`}</p>
+              </div>
               </div>
             </div>
           ))}
