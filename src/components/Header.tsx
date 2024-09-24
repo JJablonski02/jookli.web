a53abf081820a25e58c7506a2bcf5f79f2a7b8b4
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-09-24 19:02:10 +0200

    deleted comments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,42 +16,38 @@ const Header: React.FC<HeaderProps> = ({ header, headerParagraph, firstButtonLab
 
   return (
     <header>
-        <div className="mx-20 mt-14 md:mt-10 md:mx-2.5 relative">   
-        <Squares position="right">       
-        {/* <div className="-z-10 lg:hidden">
-          <span className="border-[5px] border-blue w-[381px] h-[381px] absolute rounded-[54px] rotate-45 -right-40 top-16"></span>
-          <span className="border-[5px] border-blue w-[381px] h-[381px] absolute rounded-[54px] rotate-45 -right-60 top-16"></span>
-        </div> */}
-        <div className="p-2 md:px-0 flex justify-between items-center md:flex-col">
-          <div className="md:flex md:items-center md:flex-col">
-            <h1 className="text-5xl w-10/12 md:w-full">
-              {header}
-            </h1>
-            <p className="mt-8 text-lg w-2/3 md:mt-6 md:w-full">
-              {headerParagraph}
-            </p>
-            <div className="w-2/3 mt-10 md:mx-auto md:mt-8 md:w-10/12 flex justify-start gap-6">
-              <JPButton
-                className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
-                label={firstButtonLabel}
-              />
-              <JPButton
-                className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
-                label={secondButtonLabel}
-              />
+      <div className="mx-20 mt-14 md:mt-10 md:mx-2.5 relative">
+        <Squares position="right">
+          <div className="p-2 md:px-0 flex justify-between items-center md:flex-col">
+            <div className="md:flex md:items-center md:flex-col">
+              <h1 className="text-5xl md:w-full">
+                {header}
+              </h1>
+              <p className="mt-8 text-lg md:mt-6 md:w-full">
+                {headerParagraph}
+              </p>
+              <div className="mt-10 md:mx-auto md:mt-8 md:w-10/12 flex justify-start gap-6">
+                <JPButton
+                  className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
+                  label={firstButtonLabel}
+                />
+                <JPButton
+                  className="text-primary font-PoppinsMedium p-6 bg-blue rounded-full flex flex-row gap-2 md:h-14 md:w-1/2"
+                  label={secondButtonLabel}
+                />
+              </div>
+            </div>
+            <div className="lg:hidden mr-64 h-full w-full justify-end flex">
+              {src &&
+                <Image src={src} alt="Phone image" />
+              }
+              {secondSrc &&
+                <Image className="absolute -top-12 right-44 -z-10" src={secondSrc} alt="Phone image" width={343} height={677} />
+              }
             </div>
           </div>
-          <div className="lg:hidden mr-64 h-full w-full justify-end flex">
-            {src &&
-              <Image src={src} alt="Phone image"/>
-            }
-            {secondSrc &&
-              <Image className="absolute -top-12 right-44 -z-10" src={secondSrc} alt="Phone image" width={343} height={677} />
-            }
-          </div>
-        </div>
         </Squares>
-        </div>
+      </div>
     </header>
   );
 };
