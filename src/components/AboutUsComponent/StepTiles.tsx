48600fdcd9bf11af1c6aa9086974046907643060
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-09-24 22:39:05 +0200

    added content to tiles

--- src/components/AboutUsComponent/StepTiles.tsx
+++ src/components/AboutUsComponent/StepTiles.tsx
@@ -2,32 +2,37 @@ import React from 'react'
 import StepTile from '../StepTile'
 import MachineLearning from '../../../public/MachineLearning.png'
 import phoneImage from '../../../public/HiWphone.svg'
+import { useTranslations } from 'next-intl'
 
 const AboutUsStepTiles = () => {
+    const t = useTranslations('AboutUs.StepTile')
+
     return (
         <div>
             <StepTile
-                header=''
-                paragraph=''
-                secondParagraph=''
+                header={t("firstTileHeader")}
+                paragraph={t("firstTileParagraph")}
+                secondParagraph={t("firstTileExtraParagraph")}
                 src={MachineLearning}
-                alt=''
+                alt={"Machine Learning Image"}
             />
             <StepTile
-                className='flex-row-reverse'
-                header=''
-                paragraph=''
-                secondParagraph=''
+                className="flex-row-reverse"
+                header={t("secondTileHeader")}
+                paragraph={t("secondTileParagraph")}
+                secondParagraph={t("secondTileExtraParagraph")}
                 src={MachineLearning}
-                alt=''
+                alt={"Machine Learning Image"}
             />
             <StepTile
-                header=''
-                paragraph=''
-                secondParagraph=''
+                header={t("thirdTileHeader")}
+                paragraph={t("thirdTileParagraph")}
+                secondParagraph={t("thirdTileExtraParagraph")}
                 src={phoneImage}
-                alt=''
+                alt={"Machine Learning Image"}
             />
+
+
         </div>
     )
 }
