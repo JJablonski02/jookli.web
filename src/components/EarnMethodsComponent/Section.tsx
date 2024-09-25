10da20c73b186efe405369639f42a46596c0ad81
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-09-25 16:28:23 +0200

    added classes for mobile

--- src/components/EarnMethodsComponent/Section.tsx
+++ src/components/EarnMethodsComponent/Section.tsx
@@ -11,10 +11,10 @@ const Section = () => {
     <section>
       <WidthWrapper>
         <div className="mx-20 my-40 md:mx-0">
-          <h2 className='text-4xl text-center mb-8'>{t('header')}</h2>
-          <div className="grid grid-cols-3">
+          <h2 className='text-4xl text-center mb-8 md:text-start'>{t('header')}</h2>
+          <div className="grid grid-cols-3 gap-8 lg:grid-cols-2 md:grid-cols-1">
             {SectionData.map((data, index) => (
-              <div key={index} className='m-4 p-6 rounded-2xl border-white border-2'>
+              <div key={index} className='p-6 rounded-2xl border-white border-2'>
                 <div className="flex flex-row items-center gap-4 pb-4">
                   <div className='bg-white rounded-full p-3'>
                     <Image src={data.icon} alt='icon' />
