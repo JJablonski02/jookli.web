6b80e1fa88c0d8471290e94685db5eeda895d021
JJablonski02 <kuba2805@icloud.com>
2024-10-02 18:42:20 +0200

    Naprawa kontenerów fix 1

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactNode } from "react";
 
 const PageWrapper = ({ children }: { children: ReactNode }) => {
-  return <main className="max-w-[1920px] mx-auto">{children}</main>;
+  return <main className="mx-auto max-w-[1920px]">{children}</main>;
 };
 
 export default PageWrapper;
