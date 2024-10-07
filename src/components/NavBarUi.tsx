9d7d09226e3fc84dd35ecd3004f87b1e00203765
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-10-07 18:08:17 +0200

    navbar width fix

--- src/components/NavBarUi.tsx
+++ src/components/NavBarUi.tsx
@@ -2,24 +2,24 @@
 
 import React from "react";
 import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { 
-  Link
+import {
+    Link
 } from "@nextui-org/link";
 import {
-  toHome,
-  toHowItWorks,
-  toEarnMethods,
-  toSignIn,
-  toRegister,
-  toSupport,
+    toHome,
+    toHowItWorks,
+    toEarnMethods,
+    toSignIn,
+    toRegister,
+    toSupport,
 } from "./routes";
 import LogoSvg from "../../public/logo.svg";
 import Image from "next/image";
@@ -27,76 +27,76 @@ import { useTranslations } from "next-intl";
 import JPButton from "./buttons/JPButton";
 
 type MenuItem = {
-  label: string;
-  route: string;
+    label: string;
+    route: string;
 };
 
 const NavBarUi = () => {
-  const t = useTranslations("globals.Captions");
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const t = useTranslations("globals.Captions");
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const menuItems: MenuItem[] = [
-    { label: "mainPage", route: toHome },
-    { label: "howItWorks", route: toHowItWorks },
-    { label: "earnMethods", route: toEarnMethods },
-    { label: "support", route: toSupport },
-    { label: "register", route: toRegister },
-    { label: "signIn", route: toSignIn },
-  ];
+    const menuItems: MenuItem[] = [
+        { label: "mainPage", route: toHome },
+        { label: "howItWorks", route: toHowItWorks },
+        { label: "earnMethods", route: toEarnMethods },
+        { label: "support", route: toSupport },
+        { label: "register", route: toRegister },
+        { label: "signIn", route: toSignIn },
+    ];
 
-  return (
-    <Navbar
-      className="bg-secondary max-w-[1920px] mx-auto header-height" 
-      onMenuOpenChange={setIsMenuOpen}
-      maxWidth="full"
-    >
-      <NavbarContent className="flex justify-start items-center">
-        <NavbarBrand className="max-w-[170px] min-w-[170px]">
-          <Link href="/">
-            <Image src={LogoSvg} alt="Logo jookli website" />
-          </Link>
-        </NavbarBrand>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Zamknij menu" : "Otwórz menu"}
-          className="text-primary hidden lg:block lg:h-[35px] ml-auto"
-        ></NavbarMenuToggle>
-      </NavbarContent>
+    return (
+        <Navbar
+            className="bg-secondary mx-auto header-height"
+            onMenuOpenChange={setIsMenuOpen}
+            maxWidth="full"
+        >
+            <NavbarContent className="flex justify-start items-center">
+                <NavbarBrand className="max-w-[170px] min-w-[170px]">
+                    <Link href="/">
+                        <Image src={LogoSvg} alt="Logo jookli website" />
+                    </Link>
+                </NavbarBrand>
+                <NavbarMenuToggle
+                    aria-label={isMenuOpen ? "Zamknij menu" : "Otwórz menu"}
+                    className="text-primary hidden lg:block lg:h-[35px] ml-auto"
+                ></NavbarMenuToggle>
+            </NavbarContent>
 
-      <NavbarContent className="flex lg:hidden gap-10">
-        <NavbarItem>
-          <Link href={toHome}>{t("mainPage")}</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={toHowItWorks}>{t("howItWorks")}</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={toEarnMethods}>{t("earnMethods")}</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={toSupport}>{t("support")}</Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent className="ml-4 lg:hidden" justify="end">
-        <NavbarItem>
-          <Link href={toRegister}>
-            <JPButton className="bg-blue rounded-full" label={t("register")} />
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href={toSignIn}>
-            <JPButton className="bg-blue rounded-full" label={t("signIn")} />
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarMenu className="bg-secondary text-center text-3xl font-PoppinsSemiBold pt-8">
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem className="p-2" key={`${item.label}-${index}`}>
-            <Link href={item.route}>{t(`${item.label}`)}</Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
-  );
+            <NavbarContent className="flex lg:hidden gap-10">
+                <NavbarItem>
+                    <Link href={toHome}>{t("mainPage")}</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link href={toHowItWorks}>{t("howItWorks")}</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link href={toEarnMethods}>{t("earnMethods")}</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link href={toSupport}>{t("support")}</Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent className="ml-4 lg:hidden" justify="end">
+                <NavbarItem>
+                    <Link href={toRegister}>
+                        <JPButton className="bg-blue rounded-full" label={t("register")} />
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link href={toSignIn}>
+                        <JPButton className="bg-blue rounded-full" label={t("signIn")} />
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarMenu className="bg-secondary text-center text-3xl font-PoppinsSemiBold pt-8">
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem className="p-2" key={`${item.label}-${index}`}>
+                        <Link href={item.route}>{t(`${item.label}`)}</Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </Navbar>
+    );
 };
 
 export default NavBarUi;
