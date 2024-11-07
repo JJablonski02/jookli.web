4bc473f0f835272fa1a30c84e076f47b3cdd70eb
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-11-07 16:26:23 +0100

    added authprovider to providers

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,11 +1,18 @@
 "use client"
 
 import { NextUIProvider } from "@nextui-org/system"
+import AuthProvider from "./AuthProvider"
 import { useRouter } from "next/navigation"
 
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const router = useRouter()
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+    const router = useRouter()
+    return (
+        <NextUIProvider navigate={router.push}>
+            <AuthProvider>
+                {children}
+            </AuthProvider>
+        </NextUIProvider>
+    )
 }
 
 export default Providers
