2504c5af905b8a2d41271e1ff0f86999c50dbba9
JJablonski02 <kuba2805@icloud.com>
2024-11-24 16:44:13 +0100

    sitemap, only urls

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,23 +4,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://joyprofits.com",
-      lastModified: new Date(),
-      priority: 1,
     },
     {
       url: "https://joyprofits.com/how-it-works",
-      lastModified: new Date(),
-      priority: 2,
     },
     {
       url: "https://joyprofits.com/earn-methods",
-      lastModified: new Date(),
-      priority: 2,
     },
     {
       url: "https://joyprofits.com/support",
-      lastModified: new Date(),
-      priority: 2,
     },
   ]
 }
